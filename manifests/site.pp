1ff43e0198184a859c081295f19afcf7fdafb506
ohadm2121 <menaker.ohad@gmail.com>
2018-05-15 16:04:09 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,9 +1,9 @@
 node default {
   }
   node 'ip-172-31-21-107.eu-central-1.compute.internal' {
-  incloude role::master_server
+  include role::master_server
 }
-node 'ip-172-31-29-28.eu-central-1.compute.internal' {
-incloude roll::app_server
-incloude roll::db_server
+node ip-172-31-29-28.eu-central-1.compute.internal {
+include roll::app_server
+include roll::db_server
 }
